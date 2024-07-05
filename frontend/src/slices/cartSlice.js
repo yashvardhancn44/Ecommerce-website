daa5a5565578d7cf1f048f235738553b07f476c1
yashvardhancn44 <zbti.dev@gmail.com>
2024-07-05 16:55:06 +0530

    added cart utils file and item counter in Header

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,13 +1,12 @@
 //using createSlice here as opposed to the createApi used in apiSlice as we dont have to use async functionality here. 
 
 import { createSlice } from "@reduxjs/toolkit";
+import { updateCart } from "../utils/cartUtils";
+
 
 const initialState = localStorage.getItem("cart")? JSON.parse(localStorage.getItem("cart")):{cartItems: []};
 
-//an Helper function 
-    const addDecimals = (num)=>{
-        return (Math.round(num*100)/100).toFixed(2);
-    }
+
 
 const cartSlice = createSlice({
     name: "cart",
@@ -23,24 +22,8 @@ const cartSlice = createSlice({
             } else{
                 state.cartItems = [...state.cartItems, item];
             }
-            //calculate items price
-                state.itemsPrice =addDecimals(state.cartItems.reduce((acc, item)=>acc+item.price * item.qty, 0));
-
-            // calculate shipping price (if order if over a $100 then free, else $10 shipping)
-                state.shippingPrice =addDecimals(state.itemsPrice>100?0:10);
-
-            //calculate tax price (15% tax flat)
-                state.taxPrice = addDecimals(Number((0.15*state.itemsPrice).toFixed(2)));
-
-            // calculate total price
-                state.totalPrice = (
-                    Number(state.itemsPrice)+
-                    Number(state.shippingPrice)+
-                    Number(state.taxPrice)
-                ).toFixed(2);
-
-            localStorage.setItem('cart',JSON.stringify(state));
-
+            
+            return updateCart(state);
         },
     },
 });
