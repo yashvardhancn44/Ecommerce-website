70319ce0a24cdd46ce330fff01352a8f9431070a
yashvardhancn44 <zbti.dev@gmail.com>
2024-07-05 19:54:56 +0530

    added CartScreen and Remove from cart functionality

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -15,19 +15,26 @@ const cartSlice = createSlice({
         addToCart: (state, action)=>{
             const item = action.payload;
 
-            const existItem = state.cartItems.find((x)=>x._id===item);
+            const existItem = state.cartItems.find((x)=> x._id === item._id);
 
             if(existItem){
-                    state.cartItems = state.cartItems.map((x)=>x._id === existItem._id? item:x );
+                    state.cartItems = state.cartItems.map((x)=> 
+                        x._id === existItem._id? item : x );
             } else{
                 state.cartItems = [...state.cartItems, item];
             }
             
             return updateCart(state);
         },
+        removeFromCart: (state, action)=>{
+            state.cartItems= state.cartItems.filter((x)=> x._id !== action.payload);
+            // we are returning all the items which we are not deleting. 
+
+            return updateCart(state);
+        }
     },
 });
 
-export const {addToCart} = cartSlice.actions;
+export const {addToCart, removeFromCart} = cartSlice.actions;
 
 export default cartSlice.reducer;
