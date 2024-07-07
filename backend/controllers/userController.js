c499d565868b97b2522278f7bf0d95fa72460616
yashvardhancn44 <zbti.dev@gmail.com>
2024-07-07 11:26:08 +0530

    added user routes and user controllers

---
+++ backend/controllers/userController.js
@@ -0,0 +1,80 @@
+import asyncHandler from "../middleware/asyncHandler.js";
+import User from '../models/userModel.js';
+
+
+
+// @desc: Auth user and get token
+// @route: POST /api/users/login
+// @access: public
+const authUser = asyncHandler( async (req, res)=>{
+    res.send('auth user');
+});
+
+// @desc: Register user 
+// @route: POST /api/users
+// @access: public
+const registerUser = asyncHandler( async (req, res)=>{
+    res.send('register user');
+});
+
+// @desc: Logout user & clear the cookie
+// @route: POST /api/users/logout
+// @access: private
+const logoutUser = asyncHandler( async (req, res)=>{
+    res.send('logout user');
+});
+
+// @desc: Get user profile
+// @route: GET /api/users/profile
+// @access: private
+const getUserProfile = asyncHandler( async (req, res)=>{
+    res.send('get user profile');
+});
+
+// @desc: update user profile
+// @route: PUT /api/users/profile
+// @access: private
+const updateUserProfile = asyncHandler( async (req, res)=>{
+    res.send('user profile updated');
+});
+
+// @desc: get users
+// @route: GET /api/users
+// @access: private/admin
+const getUsers = asyncHandler( async (req, res)=>{
+    res.send('get users');
+});
+
+// @desc: get user by ID
+// @route: GET /api/users/:id
+// @access: private/admin
+const getUserByID = asyncHandler( async (req, res)=>{
+    res.send('get user by ID');
+});
+
+// @desc: delete user
+// @route: DELETE /api/users/:id
+// @access: private/admin
+const deleteUser = asyncHandler( async (req, res)=>{
+    res.send('delete user');
+});
+
+// @desc: update user
+// @route: PUT /api/users/:id
+// @access: private/admin
+const updateUser = asyncHandler( async (req, res)=>{
+    res.send('update user');
+});
+
+
+export {
+    authUser,
+    registerUser,
+    logoutUser,
+    getUserProfile,
+    updateUserProfile,
+    getUsers,
+    getUserByID,
+    deleteUser,
+    updateUser,
+}
--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,8 @@ import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+
 const port = process.env.PORT || 5000;
 
 
@@ -20,6 +22,8 @@ app.get('/', (req, res)=>{
 });
 
 app.use('/api/products',productRoutes);
+app.use('/api/users', userRoutes);
+
 app.use(notFound);
 app.use(errorHandler);
 
