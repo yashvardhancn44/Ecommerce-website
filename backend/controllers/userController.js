dc5dd4cc6bf356422d0b6b82eb9114fede14179e
yashvardhancn44 <zbti.dev@gmail.com>
2024-07-07 12:30:13 +0530

    added JWT HTTP only cookie

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import User from '../models/userModel.js';
+import jwt from 'jsonwebtoken';
 
 
 
@@ -10,6 +11,18 @@ const authUser = asyncHandler( async (req, res)=>{
     const {email, password} = req.body;
     const user = await User.findOne({email: email})
     if(user && (await user.matchPassword(password))){
+        const token = jwt.sign(
+            {userid: user._id},
+            process.env.JWT_SECRET,
+            {expiresIn: '30d'}            
+        )
+        // set JWT token as a HTTP only cookie
+        res.cookie('jwt', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV !== 'development',
+            sameSite: 'strict', //prevents attacks
+            maxAge: 30*24*60*60*1000
+        });
         res.json({
             _id:user._id,
             name:user.name,
