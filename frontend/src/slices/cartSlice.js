bc08053cb62d30700ff1c80fc831baf183c7fc57
yashvardhancn44 <zbti.dev@gmail.com>
2024-07-08 19:50:59 +0530

    Created ProuctScreen and added functionality to create order

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -4,9 +4,7 @@ import { createSlice } from "@reduxjs/toolkit";
 import { updateCart } from "../utils/cartUtils";
 
 
-const initialState = localStorage.getItem("cart")? JSON.parse(localStorage.getItem("cart")):{cartItems: [], shippingAddress:{}, paymentmethod: 'PayPal'};
-
-
+const initialState = localStorage.getItem("cart")? JSON.parse(localStorage.getItem("cart")):{cartItems: [], shippingAddress:{}, paymentMethod: 'PayPal'};
 
 const cartSlice = createSlice({
     name: "cart",
@@ -37,7 +35,7 @@ const cartSlice = createSlice({
             return updateCart(state);
         },
         savePaymentMethod: (state, action)=>{
-            state.paymentmethod = action.payload;
+            state.paymentMethod = action.payload;
             return updateCart(state);
         },
         
